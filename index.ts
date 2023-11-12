420cf0bd6cfba798ee9b0455c7ee34f51a6f35c3
Sebin CM <sebinsebzz2002@gmail.com>
2023-11-12 17:22:22 +0530

    feat: add read from file

--- index.ts
+++ index.ts
@@ -2,10 +2,34 @@ import Parser from "./frontend/parser";
 import { setupGlobalEnv } from "./runtime/environment";
 import { evaluate } from "./runtime/interpreter"
 
+async function fileRead(filename: string): Promise<string> {
+    const extension = filename.split(".").pop()
+    if (extension !== "haki") {
+        throw new Error("Invalid file extension")
+    }
 
-function main() {
+    const fileContents = Bun.file(filename)
+    const arrayBuffer = await fileContents.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+
+    return buffer.toString()
+}
+
+
+async function main() {
+    let fileContents
+    if (Bun.argv[2]) {
+        fileContents = await fileRead(Bun.argv[2])
+    }
     const parser = new Parser()
     const env = setupGlobalEnv()
+
+    if (fileContents) {
+        const ast = parser.produceAST(fileContents)
+        const result = evaluate(ast, env)
+        console.log(result)
+        return
+    }
 
     console.log("haki_script")
 
